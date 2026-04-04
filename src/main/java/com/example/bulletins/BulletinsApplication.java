66729eef7caea5feaf39a4e1e499698bd789cb13
Haruka Kamogawa <yachuanxuan@gmail.com>
2026-04-04 18:50:50 +0900

    first commit

---
+++ src/main/java/com/example/bulletins/BulletinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulletins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinsApplication.class, args);
+	}
+
+}
